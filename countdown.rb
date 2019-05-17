da2ba3d28532c2d63e11fb3941575c1e464d4f03
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-17 17:47:11 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+def countdown(start)
+  count_down = 0
+  while start > count_down
+    puts "#{start} SECOND(S)!"
+    start -= 1
+    end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(start_two)
+  count_down = 0
+  while start_two > count_down
+    puts "#{start_two} SECOND(S)!"
+    start_two -= 1
+    sleep(1)
+    end
+  return "HAPPY NEW YEAR!"
 end
