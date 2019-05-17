3c96f5fafe399e13433ca51f54d09a40d86816a6
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-17 17:45:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 #write your code here
 def countdown(start)
-  count_down = 1
+  count_down = 0
   while start > count_down
     puts "#{start} SECOND(S)!"
     start -= 1
@@ -9,7 +9,7 @@ def countdown(start)
 end
 
 def countdown_with_sleep(start_two)
-  count_down = 1
+  count_down = 0
   while start_two > count_down
     puts "#{start_two} SECOND(S)!"
     start_two -= 1
